4eedbffa7ff5f367db639f8a5714d7e6310068e6
Artyom Zhukov <artyom.zhukov@progforce.com>
2020-11-19 11:27:38 +0300

    api key

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 export const config = {
     'app_id' : 'I3y79XPRH18AkcaaHGUq',
-    'apikey': 'twnqNi4bfvWp_vfQbFW030ZU5I_-Qk7rmVWwfNaHixo'
+    'apikey': 'O-898AE-vA9fkPcPuMrP86y96g27hwT0O9oxXTWccmA'
 }
